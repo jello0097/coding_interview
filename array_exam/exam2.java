6f3c0f00600649e5d26cabe5b4fe32d4b4b4e0af
jerry <jello0097@hanmail.net>
2020-04-30 05:11:35 +0900

    [배열과 문자열] 연습문제02 📌

--- array_exam/exam2.java
+++ array_exam/exam2.java
@@ -1,20 +1,29 @@
+import java.util.Arrays;
+
+
 /**
  *  문자열 두 개가 주어졌을 때 이 둘이 서로 순열 관계에 있는지 확인하는 메서드 작성 
- *  대소문자 구분, 공백없음
+ *  대소문자 구분, 공백도 하나의 문자로 침 
  */
 class Exam2 {
     //mine
-    static boolean isPermutation(final String[] strArrA, final String[] strArrB) {
-        if(strArrA.length != strArrB.length){
+    static boolean isPermutation(String[] strArrA, String[] strArrB) {
+        // 길이체크 
+        if(strArrA.length != strArrB.length)
             return false;
-        }
-        // 순열 관계 체크 --- 알고리즘 추가하기
+        // 정렬 
+        Arrays.sort(strArrA);
+        Arrays.sort(strArrB);
+        
+        if(strArrA != strArrB)
+            return false;
+        
         return true;
     }
 
     public static void main(final String[] args) {
-        final String[] a = { "123", "342", "2333", "book", "MIHEE", "MI" };
-        final String[] b = { "3323", "PEN", "BOOK", "4687", "MIHEE", "TEST" };
+        String[] a = { "123", "342", "2333", "book", "MIHEE", "MI" };
+        String[] b = { "3323", "PEN", "BOOK", "4687", "MIHEE", "TEST" };
         System.out.println(isPermutation(a,b));
     }    
 }
